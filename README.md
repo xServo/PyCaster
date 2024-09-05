e34bcf17a0d541678b4ca8cf4d0bb448645c52fc
Ryan Blachowicz <ryansblachowicz@gmail.com>
2024-09-05 10:08:47 -0400

    mac doc and multi rays

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-**PyCaster** is a raycasting renderer using pygame.
+**PyCaster** is a raycasting renderer using PyGame.
 
 
 ## Dependencies
 + Python 3
 + PyGame
-```pip install pygame```
 
+```pip3 install pygame```
 
 ## To Run
+```python3 app.py```
+
+### Installing PyGame on MacOS
+This is not straightforward like other operating systems. A virtual environment is required for install. 
+
+From the PyCaster directory:
+
+```python -m venv pygame_install```
+
+```source pygame_install/bin/activate```
+
+```pip install pygame```
+
+```deactivate``` to exit venv
+
+#### To Run
+```source pygame_install/bin/activate```
+
 ```python app.py```
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import math
 
 
 # pycaster variables
-res = 1000
+res = 800
 border_size = 3
 line_width = 3
 cnt = 0
@@ -21,6 +21,13 @@ grid_size = int(res/len(map))
 radius = (res/grid_size) / 2
 print(grid_size)
 player_pos = pygame.Vector2(res / 2, res / 2)
+# ray settings
+theta = math.radians(0) # initial ray
+ray_count = 5
+assert ray_count % 2 == 1 # ray_count needs to be odd
+ray_count += 1 # we will do range 1, ray_count; cannot work with 0
+fov = math.radians(30)
+look_sens = 2
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((res, res))
@@ -28,6 +35,51 @@ clock = pygame.time.Clock()
 running = True
 delta_time = 0
 
+# def draw_slice(i, wall_height, slice_width):
+#   for (let j = 0; j < wallHeight; j++):
+
+def draw_grid():
+  # draw grid
+  for i in range(len(map)):
+    for j in range(len(map)):
+      if map[i][j] == 1:
+        pygame.draw.rect(screen, "red", (i * grid_size, j * grid_size, grid_size - border_size, grid_size - border_size))
+      else:
+        pygame.draw.rect(screen, "white", (i * grid_size, j * grid_size, grid_size - border_size, grid_size - border_size))
+def draw_rays():
+    # draw player
+  pygame.draw.circle(screen, "green", player_pos, 20)
+  # draw rays
+  for i in range(1, ray_count):
+    if i == math.ceil(ray_count / 2):
+      cur_theta = theta
+    else:
+      cur_theta = theta + ((fov / ray_count) * i)
+
+    end_pos_x = player_pos.x / grid_size
+    end_pos_y = player_pos.y / grid_size
+    dx = math.cos(cur_theta)
+    dy = math.sin(cur_theta)
+    # increment until a wall is hit
+    incr = 0
+    while (map[math.floor(end_pos_y)][math.floor(end_pos_x)] == 0):
+      end_pos_x += dx * 0.05
+      end_pos_y += dy * 0.05
+      incr += 1
+      if incr > 300:
+        break  
+      # distance = Math.sqrt((x - this.player.x) ** 2 + (y - this.player.y) ** 2);
+      # wall_height = 300 / distance;
+    end_pos = pygame.Vector2(end_pos_x * grid_size,end_pos_y * grid_size)
+    pygame.draw.line(screen, "blue", player_pos, end_pos, line_width)
+
+    # debug
+    # print("end x: ", end_pos_x)
+    # print("end y: ", end_pos_y)
+    # print("player x: ", player_pos.x / grid_size)
+    # print("player y: ", player_pos.y / grid_size)
+    # # minimalistic-crossplatform clear
+    # print("\n\n\n\n\n\n\n\n\n\n\n")
 
 
 
@@ -39,6 +91,7 @@ while running:
       running = False
   # poll events
   keys = pygame.key.get_pressed()
+  # movement
   if keys[pygame.K_w]:
     player_pos.y -= 300 * delta_time
   if keys[pygame.K_s]:
@@ -47,44 +100,26 @@ while running:
     player_pos.x -= 300 * delta_time
   if keys[pygame.K_d]:
     player_pos.x += 300 * delta_time
+  # aim
+  if keys[pygame.K_LEFT]:
+    theta -= look_sens * delta_time
+  if keys[pygame.K_RIGHT]:
+    theta += look_sens * delta_time
+  # other keys
   if keys[pygame.K_q]:
     running = False
+
 
   # clear
   screen.fill("black")
 
 
-  # draw grid
-  for i in range(len(map)):
-    for j in range(len(map)):
-      if map[i][j] == 1:
-        pygame.draw.rect(screen, "red", (i * grid_size, j * grid_size, grid_size - border_size, grid_size - border_size))
-      else:
-        pygame.draw.rect(screen, "white", (i * grid_size, j * grid_size, grid_size - border_size, grid_size - border_size))
-
-  # draw player
-  pygame.draw.circle(screen, "green", player_pos, 20)
-  # draw line
-  # end_pos_x = (math.floor(player_pos.x / grid_size)) + 1
-  # end_pos_y = (math.floor(player_pos.y / grid_size)) + 1
-
-  cnt += 1
-  theta = math.radians(30)
-  # rotation formula
-  end_pos_x = ((player_pos.x / grid_size) * math.cos(theta)) - ((player_pos.y / grid_size) * math.sin(theta))
-  end_pos_y = ((player_pos.x / grid_size) * math.sin(theta)) + ((player_pos.y / grid_size) * math.cos(theta))
+  # draw_grid()
+  # draw_rays()
+  
+  # draw screen
 
 
-  # end_pos = pygame.Vector2(end_pos_x * grid_size + grid_size / 2,end_pos_y * grid_size + grid_size / 2)
-  end_pos = pygame.Vector2(end_pos_x * grid_size,end_pos_y * grid_size)
-  pygame.draw.line(screen, "blue", player_pos, end_pos, line_width)
-
-  print("end x: ", end_pos_x)
-  print("end y: ", end_pos_y)
-  print("player x: ", player_pos.x / grid_size)
-  print("player y: ", player_pos.y / grid_size)
-  # minimalistic-crossplatform clear
-  print("\n\n\n\n\n\n\n\n\n\n\n")
 
   # swap buffer
   pygame.display.flip()
