d11a3cc3972af8bed5ee1d07ef3ca67a6301d864
Ryan Blachowicz <ryansblachowicz@gmail.com>
2024-09-07 17:55:59 -0400

    functioning raycaster rendering

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ view_distance = far_plane - near_plane
 border_size = 3
 line_width = 3
 cnt = 0
-isMap = True
+is_map = True
 map = [
   [1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1],
-  [1,0,0,1,0,0,0,1],
+  [1,0,0,0,1,1,1,1],
+  [1,0,0,0,0,0,0,1],
   [1,1,0,0,0,0,1,1],
   [1,0,0,0,0,0,0,1],
   [1,1,0,0,0,0,1,1],
-  [1,0,0,1,0,0,0,1],
+  [1,0,0,1,0,1,1,1],
   [1,1,1,1,1,1,1,1],
 ]
 grid_size = int(res/len(map))
@@ -28,8 +28,8 @@ print(grid_size)
 player_pos = pygame.Vector2(res / 2, res / 2)
 # ray settings
 theta = math.radians(0) # initial ray
-ray_count = 11
-assert ray_count % 2 == 1 # ray_count needs to be odd
+ray_count = 20
+assert ray_count % 2 == 0 # ray_count needs to be even
 ray_count += 1 # we will do range 1, ray_count; cannot work with 0
 fov = math.radians(30)
 look_sens = 2
@@ -44,33 +44,41 @@ running = True
 delta_time = 0
 column_size = res / ray_count
 
-# def draw_slice(i, wall_height, slice_width):
-#   for (let j = 0; j < wallHeight; j++):
+def is_in_bounds(new_x, new_y, grid_size):
+  grid_x = int(new_x / grid_size)
+  grid_y = int(new_y / grid_size)
+
+  # Check if the new position is within map bounds
+  if grid_x < 0 or grid_x >= len(map[0]) or grid_y < 0 or grid_y >= len(map):
+    return False
+
+  return map[grid_y][grid_x] == 0
+
 
 def draw_grid():
   for i in range(len(map)):
     for j in range(len(map)):
       if map[i][j] == 1:
-        pygame.draw.rect(screen, "red", (i * grid_size, j * grid_size, grid_size - border_size, grid_size - border_size))
+        pygame.draw.rect(screen, "red", (j * grid_size, i * grid_size, grid_size - border_size, grid_size - border_size))
       else:
-        pygame.draw.rect(screen, "white", (i * grid_size, j * grid_size, grid_size - border_size, grid_size - border_size))
+        pygame.draw.rect(screen, "white", (j * grid_size, i * grid_size, grid_size - border_size, grid_size - border_size))
 
 def draw_wall(column, size):
   size += wall_res / 2 # accounts for tile missing on bottom and top of screen
   column += 1 # deals with zero index multiplication problem
   # pygame.draw.rect(screen, "red", (column * column_size, //// size / wall_res, column_size, res - 2*(size / wall_res))) # needs work TODO TODO TODO
   y_offset = (wall_res-size)*(res/wall_res)
-  pygame.draw.rect(screen, "red", (column * column_size, y_offset, column_size, res - y_offset*2)) # needs work TODO TODO TODO
+  pygame.draw.rect(screen, "red", (column * column_size, y_offset, column_size+1, res - y_offset*2)) # needs work TODO TODO TODO
 
 def draw_rays():
     # draw player
   pygame.draw.circle(screen, "green", player_pos, 20)
   # draw rays
   for i in range(1, ray_count):
-    if i == math.ceil(ray_count / 2):
-      cur_theta = theta
-    else:
-      cur_theta = theta + ((fov / ray_count) * i)
+    # if i == math.ceil(ray_count / 2):
+    #   cur_theta = theta
+    # else:
+    cur_theta = theta + ((fov / ray_count) * i)
 
     end_pos_x = player_pos.x / grid_size
     end_pos_y = player_pos.y / grid_size
@@ -108,21 +116,45 @@ while running:
     elif event.type == pygame.KEYDOWN: 
       # toggle map
       if event.key == pygame.K_r:
-        if isMap:
-          isMap = False
+        if is_map:
+          is_map = False
         else:
-          isMap = True
+          is_map = True
   # poll events
   keys = pygame.key.get_pressed()
   # movement
   if keys[pygame.K_w]:
-    player_pos.y -= 300 * delta_time
+    new_x = player_pos.x + math.cos(theta) * 300 * delta_time
+    new_y = player_pos.y + math.sin(theta) * 300 * delta_time
+
+    if is_in_bounds(new_x, new_y, grid_size):
+        player_pos.x = new_x
+        player_pos.y = new_y
+
   if keys[pygame.K_s]:
-    player_pos.y += 300 * delta_time
+    new_x = player_pos.x - math.cos(theta) * 300 * delta_time
+    new_y = player_pos.y - math.sin(theta) * 300 * delta_time
+
+    if is_in_bounds(new_x, new_y, grid_size):
+        player_pos.x = new_x
+        player_pos.y = new_y
+
   if keys[pygame.K_a]:
-    player_pos.x -= 300 * delta_time
+    new_x = player_pos.x + math.sin(theta) * 300 * delta_time
+    new_y = player_pos.y - math.cos(theta) * 300 * delta_time
+
+    if is_in_bounds(new_x, new_y, grid_size):
+        player_pos.x = new_x
+        player_pos.y = new_y
+
   if keys[pygame.K_d]:
-    player_pos.x += 300 * delta_time
+    new_x = player_pos.x - math.sin(theta) * 300 * delta_time
+    new_y = player_pos.y + math.cos(theta) * 300 * delta_time
+
+    if is_in_bounds(new_x, new_y, grid_size):
+        player_pos.x = new_x
+        player_pos.y = new_y
+
   # aim
   if keys[pygame.K_LEFT]:
     theta -= look_sens * delta_time
@@ -132,22 +164,16 @@ while running:
   if keys[pygame.K_q]:
     running = False
 
-
   # clear
   screen.fill("black")
 
-
-
-
-  if isMap:
+  if is_map:
     draw_grid()
     draw_rays()
   else:
+    pygame.draw.rect(screen, "grey", (0, res/2, res, res))
     for i in range(1, ray_count):
-      if i == math.ceil(ray_count / 2):
-        cur_theta = theta
-      else:
-        cur_theta = theta + ((fov / ray_count) * i)
+      cur_theta = theta + ((fov / ray_count) * i)
 
       end_pos_x = player_pos.x / grid_size
       end_pos_y = player_pos.y / grid_size
