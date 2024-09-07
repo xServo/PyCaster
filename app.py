958d23e7bb61b0341c58504a34dd0a59a103c4be
Ryan Blachowicz <ryansblachowicz@gmail.com>
2024-09-07 17:27:56 -0400

    functions as a renderer but the rays are bad

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import pygame
 import math
+import time
 
 
 # pycaster variables
 res = 800
+near_plane = 40
+far_plane = 600
+view_distance = far_plane - near_plane
 border_size = 3
 line_width = 3
 cnt = 0
+isMap = True
 map = [
   [1,1,1,1,1,1,1,1],
   [1,1,1,1,1,1,1,1],
@@ -23,29 +28,40 @@ print(grid_size)
 player_pos = pygame.Vector2(res / 2, res / 2)
 # ray settings
 theta = math.radians(0) # initial ray
-ray_count = 5
+ray_count = 11
 assert ray_count % 2 == 1 # ray_count needs to be odd
 ray_count += 1 # we will do range 1, ray_count; cannot work with 0
 fov = math.radians(30)
 look_sens = 2
+# graphics settings
+wall_res = 20
+assert wall_res % 2 == 0 # wall_res needs to be even
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((res, res))
 clock = pygame.time.Clock()
 running = True
 delta_time = 0
+column_size = res / ray_count
 
 # def draw_slice(i, wall_height, slice_width):
 #   for (let j = 0; j < wallHeight; j++):
 
 def draw_grid():
-  # draw grid
   for i in range(len(map)):
     for j in range(len(map)):
       if map[i][j] == 1:
         pygame.draw.rect(screen, "red", (i * grid_size, j * grid_size, grid_size - border_size, grid_size - border_size))
       else:
         pygame.draw.rect(screen, "white", (i * grid_size, j * grid_size, grid_size - border_size, grid_size - border_size))
+
+def draw_wall(column, size):
+  size += wall_res / 2 # accounts for tile missing on bottom and top of screen
+  column += 1 # deals with zero index multiplication problem
+  # pygame.draw.rect(screen, "red", (column * column_size, //// size / wall_res, column_size, res - 2*(size / wall_res))) # needs work TODO TODO TODO
+  y_offset = (wall_res-size)*(res/wall_res)
+  pygame.draw.rect(screen, "red", (column * column_size, y_offset, column_size, res - y_offset*2)) # needs work TODO TODO TODO
+
 def draw_rays():
     # draw player
   pygame.draw.circle(screen, "green", player_pos, 20)
@@ -89,6 +105,13 @@ while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+    elif event.type == pygame.KEYDOWN: 
+      # toggle map
+      if event.key == pygame.K_r:
+        if isMap:
+          isMap = False
+        else:
+          isMap = True
   # poll events
   keys = pygame.key.get_pressed()
   # movement
@@ -105,7 +128,7 @@ while running:
     theta -= look_sens * delta_time
   if keys[pygame.K_RIGHT]:
     theta += look_sens * delta_time
-  # other keys
+  # quit game
   if keys[pygame.K_q]:
     running = False
 
@@ -114,10 +137,42 @@ while running:
   screen.fill("black")
 
 
-  # draw_grid()
-  # draw_rays()
+
+
+  if isMap:
+    draw_grid()
+    draw_rays()
+  else:
+    for i in range(1, ray_count):
+      if i == math.ceil(ray_count / 2):
+        cur_theta = theta
+      else:
+        cur_theta = theta + ((fov / ray_count) * i)
+
+      end_pos_x = player_pos.x / grid_size
+      end_pos_y = player_pos.y / grid_size
+      dx = math.cos(cur_theta)
+      dy = math.sin(cur_theta)
+      # increment until a wall is hit
+      incr = 0
+      while (map[math.floor(end_pos_y)][math.floor(end_pos_x)] == 0):
+        end_pos_x += dx * 0.05
+        end_pos_y += dy * 0.05
+        incr += 1
+        if incr > 300:
+          draw_wall(i, 0)
+          break  
+      end_pos_x *= grid_size
+      end_pos_y *= grid_size
+      distance = math.sqrt((player_pos.x - end_pos_x) ** 2 + (player_pos.y - end_pos_y) ** 2)
+
+      if distance != 0:
+        inverted_distance = 1 / distance
+      else:
+        inverted_distance = 5000
+
+      draw_wall(i - 1, inverted_distance*wall_res*50)
   
-  # draw screen
 
 
 
